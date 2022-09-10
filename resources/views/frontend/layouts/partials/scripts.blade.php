1624f65649a1d8eb5be733b6a9a555e582c72221
Tawhidur Noor <56801563+tawhidurnoor@users.noreply.github.com>
2022-09-11 00:59:09 +0600

    Minor UI Tweaks

--- resources/views/frontend/layouts/partials/scripts.blade.php
+++ resources/views/frontend/layouts/partials/scripts.blade.php
@@ -1,22 +1,22 @@
-<script data-cfasync="false" src="assets_frontend/js/email-decode.min.js"></script>
-<script src="assets_frontend/js/jquery.min.js"></script>
+<script data-cfasync="false" src="{{ asset('assets_frontend/js/email-decode.min.js')}}"></script>
+<script src="{{ asset('assets_frontend/js/jquery.min.js')}}"></script>
 
-<script src="assets_frontend/js/bootstrap.bundle.min.js"></script>
+<script src="{{ asset('assets_frontend/js/bootstrap.bundle.min.js')}}"></script>
 
-<script src="assets_frontend/js/meanmenu.min.js"></script>
+<script src="{{ asset('assets_frontend/js/meanmenu.min.js')}}"></script>
 
-<script src="assets_frontend/js/magnific-popup.min.js"></script>
+<script src="{{ asset('assets_frontend/js/magnific-popup.min.js')}}"></script>
 
-<script src="assets_frontend/js/owl.carousel.min.js"></script>
+<script src="{{ asset('assets_frontend/js/owl.carousel.min.js')}}"></script>
 
-<script src="assets_frontend/js/wow.min.js"></script>
+<script src="{{ asset('assets_frontend/js/wow.min.js')}}"></script>
 
-<script src="assets_frontend/js/isotope.pkgd.min.js"></script>
+<script src="{{ asset('assets_frontend/js/isotope.pkgd.min.js')}}"></script>
 
-<script src="assets_frontend/js/ajaxchimp.min.js"></script>
+<script src="{{ asset('assets_frontend/js/ajaxchimp.min.js')}}"></script>
 
-<script src="assets_frontend/js/form-validator.min.js"></script>
+<script src="{{ asset('assets_frontend/js/form-validator.min.js')}}"></script>
 
-<script src="assets_frontend/js/contact-form-script.js"></script>
+<script src="{{ asset('assets_frontend/js/contact-form-script.js')}}"></script>
 
-<script src="assets_frontend/js/main.js"></script>
+<script src="{{ asset('assets_frontend/js/main.js')}}"></script>
