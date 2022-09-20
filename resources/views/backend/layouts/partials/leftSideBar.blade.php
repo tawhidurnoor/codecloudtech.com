da209d4717ffb921409444bd39c84e6523a028e8
Tawhidur Noor <56801563+tawhidurnoor@users.noreply.github.com>
2022-09-20 13:15:53 +0600

    Admin Panel: Poppins font

--- resources/views/backend/layouts/partials/leftSideBar.blade.php
+++ resources/views/backend/layouts/partials/leftSideBar.blade.php
@@ -14,31 +14,9 @@
                 </li>
 
                 <li class="sidebar-item">
-                    <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)"
-                        aria-expanded="false">
-                        <i class="mdi mdi-settings"></i>
-                        <span class="hide-menu">Website Setup </span>
-                    </a>
-                    <ul aria-expanded="false" class="collapse first-level">
-                        <li class="sidebar-item">
-                            <a href="{{ route('admin.setup.general.index') }}" class="sidebar-link">
-                                <i class="mdi mdi-adjust"></i>
-                                <span class="hide-menu"> General Information </span>
-                            </a>
-                        </li>
-                        <li class="sidebar-item">
-                            <a href="{{ route('admin.setup.meta.index') }}" class="sidebar-link">
-                                <i class="mdi mdi-adjust"></i>
-                                <span class="hide-menu"> Global SEO </span>
-                            </a>
-                        </li>
-                        <li class="sidebar-item">
-                            <a href="{{ route('admin.setup.custom_scripts.index') }}" class="sidebar-link">
-                                <i class="mdi mdi-adjust"></i>
-                                <span class="hide-menu"> Custom Script </span>
-                            </a>
-                        </li>
-                    </ul>
+                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{ route('admin.page.index') }}"
+                        aria-expanded="false"><i class="mdi mdi-book-open-page-variant"></i><span
+                            class="hide-menu">Pages</span></a>
                 </li>
 
                 <li class="sidebar-item">
@@ -81,9 +59,31 @@
                 </li>
 
                 <li class="sidebar-item">
-                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{ route('admin.page.index') }}"
-                        aria-expanded="false"><i class="mdi mdi-book-open-page-variant"></i><span
-                            class="hide-menu">Pages</span></a>
+                    <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)"
+                        aria-expanded="false">
+                        <i class="mdi mdi-settings"></i>
+                        <span class="hide-menu">Website Setup </span>
+                    </a>
+                    <ul aria-expanded="false" class="collapse first-level">
+                        <li class="sidebar-item">
+                            <a href="{{ route('admin.setup.general.index') }}" class="sidebar-link">
+                                <i class="mdi mdi-adjust"></i>
+                                <span class="hide-menu"> General Information </span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="{{ route('admin.setup.meta.index') }}" class="sidebar-link">
+                                <i class="mdi mdi-adjust"></i>
+                                <span class="hide-menu"> Global SEO </span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="{{ route('admin.setup.custom_scripts.index') }}" class="sidebar-link">
+                                <i class="mdi mdi-adjust"></i>
+                                <span class="hide-menu"> Custom Script </span>
+                            </a>
+                        </li>
+                    </ul>
                 </li>
 
             </ul>
